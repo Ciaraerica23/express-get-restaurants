7ef00511b156cd9fc9baa59a562c70731bea440e
Ciaraerica23 <ciaraestafford@gmail.com>
2023-03-11 07:20:38 -0500

    added crud methods

--- server.js
+++ server.js
@@ -5,16 +5,47 @@ const {sequelize} = require("./db");
 
 const port = 3000;
 
-    
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));   
 
-app.get('/restaurants',async(req,res)=>{
-
-        const found = await Restaurant.findAll()
-        res.json(found)
-        //res.send('hello')
+app.get('/restaurants/:id',async(req,res)=>{
+    const id= req.params.id
+    const found = await Restaurant.findByPk(id)
+    res.json(found)
+      
       
 }
 )
+
+app.get('/restaurants',async(req,res)=>{
+    const found = await Restaurant.findAll()
+    res.json(found)
+      
+      
+})
+app.post('/restaurants',async(req,res)=>{
+    await Restaurant.create({
+    name:'Fridays',
+    location:'Dover',
+    cuisine:'appetizers'
+    })
+    const found = await Restaurant.findAll()
+    res.json(found)
+})
+
+app.post('/restaurants/add',async(req,res)=>{
+    await Restaurant.create(req.body)
+    const found = await Restaurant.findAll()
+    res.send(found)
+})
+
+app.delete('/restaurants/:id',async(req,res)=>{
+    const id = req.params.id
+    const find = await Restaurant.findByPk(id)
+    await find.destroy()
+    const found = await Restaurant.findAll()
+    res.json(found)
+})
 
 
 app.listen(port, () => {
